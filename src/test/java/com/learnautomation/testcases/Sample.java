12fefc288d90ae94ec3832722efa50386c5c4028
Siva Chaitanya S <43043749+sivareddysanivarapu@users.noreply.github.com>
2025-02-11 20:39:25 +0530

    System Parameters in POM and testing via Jenkins Maven

--- src/test/java/com/learnautomation/testcases/Sample.java
+++ src/test/java/com/learnautomation/testcases/Sample.java
@@ -1,5 +1,6 @@
 package com.learnautomation.testcases;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Sample 
@@ -8,7 +9,14 @@ public class Sample
 	@Test
 	public void sampleTest()
 	{
-		System.out.println("Sample Test Executed with git");
+		System.out.println("Sample Test Executed");
+	}
+	
+	@Parameters("TestParameter")
+	@Test
+	public void sampleTest1(String TestPara)
+	{
+		System.out.println("Sample Test Executed By Passing Parameters from  pom file with value "+TestPara);
 	}
 
 }
